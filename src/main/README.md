ac82ecc3889ae2fcb13b1154a2a77cb0d3e0bbb6
Facundo Molina <facu.molina7@gmail.com>
2020-06-17 11:05:54 -0300

    Implementing a mechanism for loading the serialized objects into the FuzzedInvariants class

---
+++ src/main/README.md
@@ -0,0 +1,7 @@
+Running DynComp:
+
+`java -cp .:$DAIKONDIR/daikon.jar daikon.DynComp DataStructures.ListTester`
+
+Running Chicory:
+
+`java -cp .:$DAIKONDIR/daikon.jar daikon.Chicory --comparability-file=ListTester.decls-DynComp DataStructures.ListTester`
--- src/main/invariant/FuzzedInvariants.java
+++ src/main/invariant/FuzzedInvariants.java
@@ -5,11 +5,21 @@ import daikon.inv.Invariant;
 import daikon.inv.InvariantStatus;
 import daikon.inv.OutputFormat;
 import daikon.inv.unary.scalar.SingleScalar;
+import java.util.Map;
+import java.util.HashMap;
 import org.checkerframework.checker.lock.qual.GuardSatisfied;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 import typequals.prototype.qual.Prototype;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.thoughtworks.xstream.XStream;
 
 /**
  * Represents a set of invariants generating by fuzzing a grammar
@@ -28,16 +38,27 @@ public class FuzzedInvariants extends PointerInvariant {
   /** Boolean. True iff Positive invariants should be considered. */
   public static boolean dkconfig_enabled = Invariant.invariantEnabledDefault;
 
+   
+  /** The HashMap mapping hashcodes to objects */
+  private Map<Integer,Object> objects;
+
+  /** The file from which the hashmap must be loaded*/
+  private static final String serialiazed_file_name = "/Users/fmolina/phd/software/daikon-5.8.2/examples/java-examples/StackAr/serializedobjects.xml";
+
   ///
   /// Required methods
   ///
 
   private FuzzedInvariants(PptSlice ppt) {
     super(ppt);
+    System.out.println("FuzzedInvs builder from PptSlice");
+    load_objects();
   }
 
   private @Prototype FuzzedInvariants() {
     super();
+    System.out.println("FuzzedInvs default builder");
+    load_objects();
   }
 
   private static @Prototype FuzzedInvariants proto = new @Prototype FuzzedInvariants();
@@ -45,6 +66,28 @@ public class FuzzedInvariants extends PointerInvariant {
   /** Returns the prototype invariant. */
   public static @Prototype FuzzedInvariants get_proto() {
     return proto;
+  }
+
+  /** Loads the hashmap containing the pairs hahdcode,objects */
+  private void load_objects() {
+    XStream xstream = new XStream();
+    Object map_obj = new HashMap<Integer,Object>();
+    try {
+      ObjectInputStream ois = xstream.createObjectInputStream(new FileInputStream(serialiazed_file_name));
+      try {
+        while (true) {
+          map_obj = ois.readObject();
+          System.out.println("Reading object input stream, this should be made only once");
+        }
+      } catch (EOFException e) {
+        /* The loop ends here */ }
+      ois.close();
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Cannot deserialize file: " + serialiazed_file_name);
+    }
+    objects = (Map<Integer,Object>) map_obj;
+    System.out.println("Loaded objects: "+objects.size());
   }
 
   /** returns whether or not this invariant is enabled */
@@ -69,8 +112,8 @@ public class FuzzedInvariants extends PointerInvariant {
   @Override
   //public InvariantStatus check_modified(long v, int count) {
   public InvariantStatus check_modified(Object v, int count) {  
-    System.out.println(v == null? "is null":v.getClass().getSimpleName());
-    System.out.println(v == null? "":v.toString());
+    //System.out.println(v == null? "is null":v.getClass().getSimpleName());
+    //System.out.println(v == null? "":v.toString());
     //System.out.println("Hey! This is the FuzzedInvariants being evaluated");
     //if (v <= 0) {
     //  return InvariantStatus.FALSIFIED;
