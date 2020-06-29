3eb97130953a7e3939fd94dc08f4e4f82ff21251
Facundo Molina <facu.molina7@gmail.com>
2020-06-29 10:54:36 -0300

    Adding empty class ExpressionEvaluator

---
+++ src/main/expression/ExpressionEvaluator.java
@@ -0,0 +1,22 @@
+package expression;
+
+/**
+ * This class represents an Expression Evaluator. Provides a method that given a Java object 
+ * and an expression (in the Alloy grammar) it evaluates the expression on the object.
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
+
+public class ExpressionEvaluator {
+
+  /**
+   * Evaluate the given Alloy expression on the given object
+   */
+	public static boolean eval(String alloy_expr, Object o) {
+		// Parse the expression and get the tree
+
+		// Evalute the expression on the object
+
+		return true;
+	}
+
+}
