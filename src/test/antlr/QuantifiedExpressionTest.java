25347f557a52540ad31af1fc626cfc5e2f4c120e
Facundo Molina <facu.molina7@gmail.com>
2020-06-29 16:42:44 -0300

    Adding junit tests

---
+++ src/test/antlr/QuantifiedExpressionTest.java
@@ -0,0 +1,51 @@
+package antlr;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+/**
+ * Provide a set of tests to ensure correct parsing of quantified expressions
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
+public class QuantifiedExpressionTest {
+  
+  private AlloyExprGrammarLexer lexer;
+  private AlloyExprGrammarParser parser;
+
+  /**
+   * Initialize the lexer and parser from the given alloy expr
+   */ 
+  private void intialize(String alloy_expr) {
+    lexer = new AlloyExprGrammarLexer(CharStreams.fromString(alloy_expr));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    parser = new AlloyExprGrammarParser(tokens);
+  }
+
+  @Test
+  public void test_qt_1() {
+    String qt_expr = "all n : List.*(next) : n.next != null";
+    intialize(qt_expr);
+    ParseTree tree = parser.parse();
+    assertTrue(parser.getNumberOfSyntaxErrors()==0);
+  }
+
+  @Test
+  public void test_qt_2() {
+  	String qt_expr = "all n : AvlTreeList.root.^(left) : n not in n.^(right)";
+  	intialize(qt_expr);
+    ParseTree tree = parser.parse();
+    assertTrue(parser.getNumberOfSyntaxErrors()==0);
+  }
+  
+  @Test
+  public void test_bad_qt_1() {
+    String bad_qt_expr = "badoperator n : List.*(next) : n.next != null";
+    intialize(bad_qt_expr);
+    ParseTree tree = parser.parse();
+    assertTrue(parser.getNumberOfSyntaxErrors() > 0);
+  }
+
+}
