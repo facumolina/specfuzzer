57a82ed072e3f85cd63c9d95926efd79b7a3337f
Facundo Molina <facu.molina7@gmail.com>
2020-07-16 15:24:34 -0300

    Implementation of repOk for class List

--- src/examples/DataStructures/List.java
+++ src/examples/DataStructures/List.java
@@ -1,6 +1,8 @@
 package DataStructures;
 
-import java.io.Serializable; 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
@@ -32,12 +34,30 @@ public class List implements Serializable {
     }
   }
 
+  public boolean repOk() {
+    Set<List> visited = new HashSet<List>();
+    List curr = this;
+    while (curr.x != SENTINEL) {
+      // The list should acyclic
+      if (!visited.add(curr))
+        return false;
+      // The list should be sorted
+      List curr_next = curr.next;
+      if (curr.x > curr_next.x)
+        return false;
+
+      curr = curr_next;
+    }
+    return true;
+  }
+
   @Override
-  public String toString(){
+  public String toString() {
     if (x == SENTINEL) {
       return "null";
     } else {
       return x + ", " + next.toString();
     }
   }
+
 }
