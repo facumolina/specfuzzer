70a1195c1bf9471ecb9c3575b65f91f0a9899ec7
Facundo Molina <facu.molina7@gmail.com>
2020-07-17 15:30:55 -0300

    Implementing the java Fuzzers (actually, is a wrapper to call the python Fuzzers)

---
+++ src/main/fuzzer/BasicFuzzer.java
@@ -0,0 +1,13 @@
+package fuzzer;
+
+/**
+ * This class implements a Basic Fuzzer based on the Simple Grammar Fuzzer from The Fuzzing Book
+ * (https://www.fuzzingbook.org/)
+ * 
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
+public class BasicFuzzer {
+
+  // @ TODO
+  // Implement this class and make the Fuzzer class an abstract one.
+}
---
+++ src/main/fuzzer/Fuzzer.java
@@ -0,0 +1,38 @@
+package fuzzer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+/**
+ * This class represents a Fuzzer. Currently, this implementation is only used to communicate with a
+ * Python fuzzer.
+ * 
+ * 
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ *
+ */
+public class Fuzzer {
+
+  private static final String fuzzer_script = System.getProperty("user.dir") + "/FuzzSpecs.py";
+
+  /**
+   * Returns an expression of the given grammar produced with the Python fuzzer.
+   */
+  public static String fuzz(String grammar_file) {
+    try {
+      Process p = Runtime.getRuntime()
+          .exec(new String[] { "python", fuzzer_script, grammar_file, Integer.toString(1) });
+      p.waitFor();
+      OutputStream os = p.getOutputStream();
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      return br.readLine();
+
+    } catch (Exception e) {
+      throw new IllegalArgumentException(
+          "Unable to run script FuzzSpecs.py for grammar " + grammar_file);
+    }
+  }
+
+}
