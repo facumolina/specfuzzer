f5912ab0bf45018c5bd51f5b3a1a41320ed0ee0b
Facundo Molina <facu.molina7@gmail.com>
2020-07-22 00:30:17 -0300

    Script for test generations include parameter to produce a driver expected by Daikon

--- gen-tests.sh
+++ gen-tests.sh
@@ -5,5 +5,6 @@ test_class_name=$2;
 
 echo '> Generating tests for class '$target_class
 
-java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=100 --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --regression-test-basename=$test_class_name
+java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=100 --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --junit-reflection-allowed=false --regression-test-basename=$test_class_name
+
 echo '> Done!'
