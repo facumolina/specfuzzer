8dd1c9b5c120697d6fc4d50c9efbb2fa22993d6f
Facundo Molina <facu.molina7@gmail.com>
2020-07-22 00:03:26 -0300

    Script for automated test generation with Randoop parameterized

--- gen-tests.sh
+++ gen-tests.sh
@@ -1,2 +1,9 @@
-java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=DataStructures.List --output-limit=100 --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --regression-test-basename=ListTester
+#!/bin/bash
 
+target_class=$1;
+test_class_name=$2;
+
+echo '> Generating tests for class '$target_class
+
+java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=100 --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --regression-test-basename=$test_class_name
+echo '> Done!'
