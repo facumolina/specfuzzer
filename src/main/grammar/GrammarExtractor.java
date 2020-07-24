a04436a5d7fbc09f3c62ca54ca3c17d53f5ef6f7
Facundo Molina <facu.molina7@gmail.com>
2020-07-24 15:47:50 -0300

    The vertexs of the type graph now are Class objects instead of Strings

--- src/main/grammar/GrammarExtractor.java
+++ src/main/grammar/GrammarExtractor.java
@@ -18,7 +18,7 @@ import org.json.simple.JSONObject;
 public class GrammarExtractor {
 
   // Type graph of the CUT
-  private static Graph<String, LabeledEdge> type_graph;
+  private static Graph<Class<?>, LabeledEdge> type_graph;
   private static int bound = 3;
   private static String GRAMMARS_DIR = System.getProperty("user.dir") + "/grammars/";
 
@@ -38,7 +38,7 @@ public class GrammarExtractor {
 
     // Build the corresponding type graph
     System.out.println("Building the Type Graph");
-    type_graph = new DirectedPseudograph<String, LabeledEdge>(LabeledEdge.class);
+    type_graph = new DirectedPseudograph<Class<?>, LabeledEdge>(LabeledEdge.class);
     build_type_graph(cut, new HashSet<String>());
     System.out.println("Nodes: " + type_graph.vertexSet().toString());
     System.out.println("Edges: " + type_graph.edgeSet().toString());
@@ -56,7 +56,7 @@ public class GrammarExtractor {
    */
   private static void build_type_graph(Class<?> cut, Set<String> visited) {
     if (visited.add(cut.getName())) {
-      type_graph.addVertex(cut.getSimpleName());
+      type_graph.addVertex(cut);
       // Get the fields of the given type
       Set<Class<?>> to_visit = new HashSet<Class<?>>();
       List<Field> fields = getAllFields(new LinkedList<Field>(), cut);
@@ -65,9 +65,8 @@ public class GrammarExtractor {
           // For each non static field
           String fld_decl_name = fld.getName();
           Class<?> fld_type = fld.getType();
-          type_graph.addVertex(fld_type.getSimpleName());
-          type_graph.addEdge(cut.getSimpleName(), fld_type.getSimpleName(),
-              new LabeledEdge(fld_decl_name));
+          type_graph.addVertex(fld_type);
+          type_graph.addEdge(cut, fld_type, new LabeledEdge(fld_decl_name));
 
           to_visit.add(fld_type);
         }
@@ -97,15 +96,15 @@ public class GrammarExtractor {
    * Create the quantification related symbols from label
    */
   private static void add_quantification_symbols_from_label(Map<String, List<String>> grammar,
-      String type_name, String curr_expr, String label) {
-    GrammarBuilder.add_quantification_symbols(grammar, type_name, curr_expr, label);
-    Set<LabeledEdge> edges = type_graph.outgoingEdgesOf(type_name);
+      Class<?> cut, String curr_expr, String label) {
+    GrammarBuilder.add_quantification_symbols(grammar, cut.getSimpleName(), curr_expr, label);
+    Set<LabeledEdge> edges = type_graph.outgoingEdgesOf(cut);
     for (LabeledEdge edge : edges) {
-      String target_type = type_graph.getEdgeTarget(edge);
-      if (!type_name.equals(target_type)) {
+      Class<?> target_type = type_graph.getEdgeTarget(edge);
+      if (!cut.equals(target_type)) {
         String dest_label = edge.getLabel();
-        GrammarBuilder.add_quantification_over_field_symbols(grammar, type_name, curr_expr,
-            target_type, label, dest_label);
+        GrammarBuilder.add_quantification_over_field_symbols(grammar, cut.getSimpleName(),
+            curr_expr, target_type.getSimpleName(), label, dest_label);
       }
     }
 
@@ -123,21 +122,22 @@ public class GrammarExtractor {
   /**
    * Traverse the type graph from the given type and extend the given grammar
    */
-  private static void traverse_graph(String type_name, String curr_expr,
+  private static void traverse_graph(Class<?> cut, String curr_expr,
       Map<String, List<String>> grammar, int k) {
     if (k > 0) {
-      Set<LabeledEdge> edges = type_graph.outgoingEdgesOf(type_name);
+      Set<LabeledEdge> edges = type_graph.outgoingEdgesOf(cut);
       for (LabeledEdge edge : edges) {
-        String target_type = type_graph.getEdgeTarget(edge);
-        if (type_name.equals(target_type)) {
+        Class<?> target_type = type_graph.getEdgeTarget(edge);
+        if (cut.equals(target_type)) {
           // We have a closure case, so create the quantification related symbols
-          add_quantification_symbols_from_label(grammar, type_name, curr_expr, edge.getLabel());
+          add_quantification_symbols_from_label(grammar, cut, curr_expr, edge.getLabel());
         } else {
           // This is not a closure case, continue exploring only reference types
-          if (TypesUtil.is_reference(target_type)) {
+          if (TypesUtil.is_reference(target_type.getSimpleName())) {
             traverse_graph(target_type, curr_expr + "." + edge.getLabel(), grammar, k - 1);
           } else {
-            add_symbol_for_type(target_type, curr_expr + "." + edge.getLabel(), grammar);
+            add_symbol_for_type(target_type.getSimpleName(), curr_expr + "." + edge.getLabel(),
+                grammar);
           }
         }
       }
@@ -164,7 +164,7 @@ public class GrammarExtractor {
   private static void extract_grammar(Class<?> cut) {
     System.out.println("Extracting grammar from initial type: " + cut.getName());
     Map<String, List<String>> grammar = GrammarBuilder.create();
-    traverse_graph(cut.getSimpleName(), cut.getSimpleName(), grammar, bound);
+    traverse_graph(cut, cut.getSimpleName(), grammar, bound);
     System.out.println();
     JSONObject json_grammar = new JSONObject(grammar);
     System.out.println(json_grammar.toJSONString());
