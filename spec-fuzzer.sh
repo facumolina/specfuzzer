9c7cadd369ca8530b72133b6bbd5023d01a6e61e
Facundo Molina <facu.molina7@gmail.com>
2020-12-28 15:46:03 -0300

    Improving spec-fuzzer script

--- spec-fuzzer.sh
+++ spec-fuzzer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 grammar_file=$1;
-objects_file=$2;
-dtrace_file=$3;
-iterations=10;
+target_name=$2;
+objects_file="daikon-outputs/"$2"-objects.xml";
+dtrace_file="daikon-outputs/"$2".dtrace.gz";
+mutants_dir="daikon-outputs/mutants";
+invs_file=$target_name".inv.gz"
+
+iterations=2;
+invs_to_fuzz=10;
 
 echo '> SpecFuzzer'
 echo ''
@@ -15,8 +20,18 @@ i=1
 while [[ $i -le $iterations ]]
 do
   echo '> Running Daikon - Iteration '$i
+  java -cp build/classes/:lib/* daikon.Daikon --user-defined-invariant invariant.FuzzedInvariant --grammar-to-fuzz $grammar_file --fuzzed-invariants $invs_to_fuzz --serialiazed-objects $objects_file $dtrace_file > /dev/null 2>&1
+
   echo '> Checking invariants on Mutants - Iteration '$i
+  for mutant_dtrace in $mutants_dir"/"$target_name*.dtrace.gz; do
+    base_name=${mutant_dtrace/%$".dtrace.gz"}
+    mutant_objects_file=$base_name"-objects.xml"
+    echo '> Checking on mutant: '$mutant_dtrace
+    java -cp build/classes/:lib/* daikon.tools.InvariantChecker --conf --serialiazed-objects $mutant_objects_file $invs_file $mutant_dtrace
+    echo ''
+  done
   echo ''
+
   ((i = i + 1))
 done
 
