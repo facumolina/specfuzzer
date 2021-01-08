0bd2bae2b41305555363073fd54b9273318c4d29
Facundo Molina <facu.molina7@gmail.com>
2021-01-08 08:43:11 -0300

    Randoop generate 1000 tests per case

--- randoop-gen-tests.sh
+++ randoop-gen-tests.sh
@@ -5,6 +5,6 @@ test_class_name=$2;
 
 echo '> Generating tests for class '$target_class
 
-java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=100 --literals-level=ALL --literals-file=literals/lits.txt --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --junit-reflection-allowed=false --regression-test-basename=$test_class_name
+java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=1000 --literals-level=ALL --literals-file=literals/lits.txt --omit-methods='repOk|toString|getClass' --junit-package-name='testers' --junit-output-dir=src/examples/ --junit-reflection-allowed=false --regression-test-basename=$test_class_name
 
 echo '> Done!'
