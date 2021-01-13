4b226a177cd50b6adb5b88786d3720b35c164cf3
Facundo Molina <facu.molina7@gmail.com>
2021-01-13 08:58:16 -0300

    Update list-setup script with proper comments

--- experiments/setup/list-setup.sh
+++ experiments/setup/list-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script allows to perform all the steps of the fuzzing based specification inference technique for the List case study. 
+# This script allows to perform valid and invalid trace generation to enable the fuzzing based specification inference technique for the List case study. 
 
 # Verify that the required environment variables have been set
 [[ -z "$DAIKONDIR" ]] && { echo "> The environment variable DAIKONDIR is empty" ; exit 1; }
@@ -9,16 +9,18 @@
 echo '> Starting experiment for case: DataStructure.List'
 echo ''
 
-# First run all the Daikon steps
+# Perform the Dynamic Comparability Analysis
 echo '> Performing Dynamic Comparability Analysis from driver: testers.ListTesterDriver'
 java -cp build/classes/:lib/daikon.jar daikon.DynComp testers.ListTesterDriver --output-dir=$SPECFUZZER/daikon-outputs
 echo ''
 
+# Run Chicory on the existing testsuite to create the valid trace 
 echo '> Running Chicory for dtrace generation from driver: testers.ListTesterDriver'
 java -cp build/classes/:lib/daikon.jar daikon.Chicory --output-dir=$SPECFUZZER/daikon-outputs/ --comparability-file=$SPECFUZZER/daikon-outputs/ListTesterDriver.decls-DynComp --ppt-omit-pattern='ListTester.*' testers.ListTesterDriver daikon-outputs/ListTesterDriver-objects.xml
 echo 'Objects saved in file: '$SPECFUZZER'/daikon-outputs/ListTesterDriver-objects.xml'
 echo ''
 
+# Use Major to create the mutated traces
 echo '> Generating mutants with MAJOR'
 ./experiments/setup/gen-mutated-traces.sh DataStructures/List.java ListTester
 echo ''
