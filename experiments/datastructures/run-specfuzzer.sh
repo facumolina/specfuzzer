2374394fdc726ad7401a4632df1bbccf03ad770c
Facundo Molina <facu.molina7@gmail.com>
2021-02-03 15:48:27 -0300

    Specfuzzer is run on a separate file

---
+++ experiments/datastructures/run-specfuzzer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dtrace=$1
+objects=$2
+grammar=$3
+invstofuzz=$4
+invfile=$5
+class=$6
+method=$7
+outputfile=$8
+
+echo '> Running Daikon + SpecFuzzer on dtrace file: '$dtrace
+
+java -cp build/classes/:lib/* daikon.Daikon --user-defined-invariant invariant.FuzzedInvariant --grammar-to-fuzz $grammar --fuzzed-invariants $invstofuzz --serialiazed-objects $objects $dtrace
+
+rm -f $outputfile
+echo ''
+echo '> Writing output to file: '$outputfile
+java -cp build/classes/:lib/* daikon.PrintInvariants $invfile --ppt-select '.'$class':::OBJECT' > $outputfile
+java -cp build/classes/:lib/* daikon.PrintInvariants $invfile --ppt-select '.'$method'.' >> $outputfile
+
+echo '> Output written.'
+
