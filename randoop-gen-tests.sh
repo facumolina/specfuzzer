348f3a84e5915c744fd61b2c72dde4407fcc0b9a
Facundo Molina <facu.molina7@gmail.com>
2021-02-04 17:11:51 -0300

    Do not include repOK when generating tests

--- randoop-gen-tests.sh
+++ randoop-gen-tests.sh
@@ -5,6 +5,6 @@ test_class_name=$2;
 
 echo '> Generating tests for class '$target_class
 
-java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=1000 --literals-level=ALL --literals-file=literals/lits.txt --omit-methods='repOk|toString|getClass|parallelStream|listIterator|spliterator|stream|iterator' --junit-package-name='testers' --junit-output-dir=src/examples/ --junit-reflection-allowed=false --regression-test-basename=$test_class_name --no-regression-assertions=true
+java -cp build/classes/:lib/randoop-all-4.2.4.jar randoop.main.Main gentests --testclass=$target_class --output-limit=1000 --literals-level=ALL --literals-file=literals/lits.txt --omit-methods='repOk|repOK|toString|getClass|parallelStream|listIterator|spliterator|stream|iterator|contains|containsAll|equals|retainAll|lastIndexOf|toArray' --junit-package-name='testers' --junit-output-dir=src/examples/ --junit-reflection-allowed=false --regression-test-basename=$test_class_name --no-regression-assertions=true
 
 echo '> Done!'
