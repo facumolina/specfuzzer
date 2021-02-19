b39fb4d006021e90775ce312ec9cc5779197c136
Facundo Molina <facu.molina7@gmail.com>
2021-02-19 11:39:55 -0300

    Improve SpecFuzzer script on SF110 project

--- experiments/sf110/run-specfuzzer.sh
+++ experiments/sf110/run-specfuzzer.sh
@@ -23,7 +23,11 @@ tests_dir=$method_dir/2/tests
 results_dir=experiments/sf110/$sf110_project
 objects_file=$results_dir/$class_name-$method_name-objects.xml
 dtrace_file=$results_dir/$class_name-$method_name.dtrace.gz
+mutants_dir=$results_dir/mutants
+target_name=$class_name-$method_name
 
+# Fuzzing vars
+invs_to_fuzz=100;
 
 # Start
 echo '> SpecFuzzer'
@@ -33,8 +37,30 @@ echo '> Target: '$sf110_project'/'$fqname$'.'$method_name
 cp_for_tests_compilation=$project_sources/build/classes/:$project_sources/lib/*:$EVOSPEX/lib/hamcrest-core-1.3.jar:$EVOSPEX/lib/junit-4.12.jar
 cp_for_daikon=build/classes/:lib/*:$cp_for_tests_compilation:$tests_dir/build/classes/
 
+# Clean file for this step to work properly
+cp base_invs_file.xml invs_file.xml
+cp base-invs-by-mutants.csv invs-by-mutants.csv
+
 # Run the SpecFuzzer
-java -cp $cp_for_daikon daikon.Daikon --user-defined-invariant invariant.FuzzedInvariant --grammar-to-fuzz $grammar_to_fuzz --fuzzed-invariants 100 --serialiazed-objects $objects_file $dtrace_file
+java -cp $cp_for_daikon daikon.Daikon --user-defined-invariant invariant.FuzzedInvariant --grammar-to-fuzz $grammar_to_fuzz --living-fuzzed-invariants invs_file.xml --fuzzed-invariants $invs_to_fuzz --serialiazed-objects $objects_file $dtrace_file
+
+invs_file=$target_name'.inv.gz'
+
+echo '> Checking invariants on Mutants'
+for mutant_dtrace in $mutants_dir"/"$target_name*.dtrace.gz; do
+  base_name=${mutant_dtrace/%$".dtrace.gz"}
+  mutant_objects_file=$base_name"-objects.xml"
+  echo '> Checking on mutant: '$mutant_dtrace
+  java -cp $cp_for_daikon daikon.tools.InvariantChecker --conf --serialiazed-objects $mutant_objects_file $invs_file $mutant_dtrace
+  echo ''
+  echo '> Saving mutants results file'
+  python3 single-mutant-result.py invs.csv 1 $mutant_dtrace
+
+done
+echo ''
+
+echo '> Rating living invariants by mutation killing ability'
+python3 process-final-results.py invs-by-mutants.csv
 
 echo '> Results saved in '$results_dir
 
