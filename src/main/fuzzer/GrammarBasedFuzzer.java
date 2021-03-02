578d61de05325c6b545cf8dcf527f65fb15e2b73
Facundo Molina <facu.molina7@gmail.com>
2021-03-02 15:59:11 -0300

    Adding missing comment

--- src/main/fuzzer/GrammarBasedFuzzer.java
+++ src/main/fuzzer/GrammarBasedFuzzer.java
@@ -2,6 +2,10 @@ package fuzzer;
 
 import org.json.simple.JSONObject;
 
+/**
+ * Abstract class representing a Grammar based Fuzzer
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
 public abstract class GrammarBasedFuzzer extends Fuzzer {
 
   protected JSONObject grammar; // Grammar to fuzz
