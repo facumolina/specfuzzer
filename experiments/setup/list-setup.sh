0c2aea707ec6b079de247db514f5a9011001af94
Facundo Molina <facu.molina7@gmail.com>
2021-03-18 11:57:57 -0300

    Modularize setup script

--- experiments/setup/list-setup.sh
+++ experiments/setup/list-setup.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-# This script allows to perform valid and invalid trace generation to enable the fuzzing based specification inference technique for the List case study. 
+# Setup for DataStructures.List class 
 
-# Verify that the required environment variables have been set
-[[ -z "$DAIKONDIR" ]] && { echo "> The environment variable DAIKONDIR is empty" ; exit 1; }
-[[ -z "$SPECFUZZER" ]] && { echo "> The environment variable SPECFUZZER is empty" ; exit 1; }
+# Define vars
+fqname='DataStructures.List'
+tester_base='ListTester'
+omit_pattern=$tester_base'.*'
+driver_name='ListTesterDriver'
+driver='testers.ListTesterDriver'
+daikon_out=$SPECFUZZER'/daikon-outputs'
+cmp_file=$daikon_out'/ListTesterDriver.decls-DynComp'
+objs_file='daikon-outputs/ListTesterDriver-objects.xml'
+source_file='DataStructures/List.java'
 
-echo '> Starting experiment for case: DataStructure.List'
-echo ''
-
-# Perform the Dynamic Comparability Analysis
-echo '> Performing Dynamic Comparability Analysis from driver: testers.ListTesterDriver'
-java -cp build/classes/:lib/daikon.jar daikon.DynComp testers.ListTesterDriver --output-dir=$SPECFUZZER/daikon-outputs
-echo ''
-
-# Run Chicory on the existing testsuite to create the valid trace 
-echo '> Running Chicory for dtrace generation from driver: testers.ListTesterDriver'
-java -cp build/classes/:lib/daikon.jar daikon.Chicory --output-dir=$SPECFUZZER/daikon-outputs/ --comparability-file=$SPECFUZZER/daikon-outputs/ListTesterDriver.decls-DynComp --ppt-omit-pattern='ListTester.*' testers.ListTesterDriver daikon-outputs/ListTesterDriver-objects.xml
-echo 'Objects saved in file: '$SPECFUZZER'/daikon-outputs/ListTesterDriver-objects.xml'
-echo ''
-
-# Use Major to create the mutated traces
-echo '> Generating mutants with MAJOR'
-./experiments/setup/gen-mutated-traces.sh DataStructures/List.java ListTester
-echo ''
-
-echo '> Done!'
+./experiments/setup/common.sh $fqname $tester_base $driver_name $source_file
+ 
