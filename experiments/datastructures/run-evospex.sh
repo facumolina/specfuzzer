7c52cba758d8eeea04fd9e1174b6cfc46eac1d83
Facundo Molina <facu.molina7@gmail.com>
2021-03-21 15:09:06 -0300

    Adding evospex runner script

---
+++ experiments/datastructures/run-evospex.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Verify that the required environment variables have been set
+[[ -z "$EVOSPEX" ]] && { echo "> The environment variable EVOSPEX is empty" ; exit 1; }
+
+# Get arguments
+root_dir=$EVOSPEX;
+fqname=$1
+method=$2
+objs_folder=$3
+
+evospex_jar=$EVOSPEX/target/evospex-jar-with-dependencies.jar
+specfuzzer_jar=$SPECFUZZER/dest/jar/FuzzSpecs.jar
+
+echo '> Running EvoSpex on subject: '$fqname'.'$method
+
+# Get required variables
+target_class_sn=$(sed 's/.*\.//' <<< $1)
+resources_base=$EVOSPEX'/src/test/resources';
+model_base=$resources_base'/objects'
+model_file=$model_base/$target_class_sn/model/$target_class_sn.als
+base_folder=$model_base/$objs_folder;
+cd $base_folder;
+num_args="$( ls -l in*.xml | wc -l )";
+num_outputs="$( ls -l out*.xml | wc -l )";
+cd $root_dir;
+echo 'model: '$model_file
+echo 'base_folder: '$base_folder
+echo 'num_args: '$num_args
+echo 'num_outputs: '$num_outputs
+
+# Run EvoSpex
+java -cp $specfuzzer_jar:$evospex_jar main.EvoSpex $model_file $base_folder $num_args $num_outputs $3 $4 $5 $6 $7 $8
+
