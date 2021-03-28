773bceb2cf75d6b52fef325c8b8cc8dbb9753711
Facundo Molina <facu.molina7@gmail.com>
2021-03-27 21:12:30 -0300

    Final results are shown more friendly

--- process-final-results.py
+++ process-final-results.py
@@ -19,24 +19,26 @@ print("Unique mutants: "+str(len(unique_mut)))
 print()
 
 column_names = ["invariant", "fails", "fst_it", "total_its"]
-final_df = pd.DataFrame(columns = column_names)
 
-for index, row in df.iterrows():
-    if final_df[final_df['invariant']==row['invariant']].empty:
-        min_it = df[df['invariant']==row['invariant']]['iteration'].min()
-        total_it = len(df[df['invariant']==row['invariant']]['iteration'].unique())
-        fails = len(df[(df['invariant']==row['invariant']) & (df['iteration']==min_it)])
-        row_to_add = {
-                        "invariant":[row['invariant']],
-			"fails":[fails],
-                        "fst_it":[min_it],
-                        "total_its":[total_it]
-                     }
-        final_df = final_df.append(pd.DataFrame(row_to_add), ignore_index=True)
 
-print("Invariants sorted by mutation killing ability (times that failed in program points)")
-ka = final_df.sort_values(by=['fails'], ascending=False, ignore_index=True)
+for ppt in unique_ppt:
+    ppt_df = pd.DataFrame(columns = column_names)
+    for index, row in df.iterrows():
+        if (row['ppt']==ppt and ppt_df[ppt_df['invariant']==row['invariant']].empty):
+            min_it = df[df['invariant']==row['invariant']]['iteration'].min()
+            total_it = len(df[df['invariant']==row['invariant']]['iteration'].unique())
+            fails = len(df[(df['invariant']==row['invariant']) & (df['iteration']==min_it)])
+            row_to_add = {
+            "invariant":[row['invariant']],
+            "fails":[fails],
+            "fst_it":[min_it],
+            "total_its":[total_it]
+            }
+            ppt_df = ppt_df.append(pd.DataFrame(row_to_add), ignore_index=True)
 
-pd.set_option('max_colwidth', 800)
-
-print(ka)
+    print("Program Point: "+ppt)
+    print("     Invariants sorted by mutation killing ability (times that failed in the program point)")
+    ka = ppt_df.sort_values(by=['fails'], ascending=False, ignore_index=True)
+    pd.set_option('max_colwidth', 800)
+    print(ka[['invariant','fails']])
+    print()
