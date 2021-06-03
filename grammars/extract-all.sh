c956886d3c5ff7d9d640304263a3784e6f5efe13
Facundo Molina <facu.molina7@gmail.com>
2021-06-03 18:02:58 -0300

    All classes used in tests are included in extractor script

---
+++ grammars/extract-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Extract all the grammars that are used to test the system
+java -cp "lib/*" grammar.GrammarExtractor gassert.SimpleMethods
+java -cp "lib/*" grammar.GrammarExtractor DataStructures.List
+java -cp "lib/*" grammar.GrammarExtractor DataStructures.AvlTreeList
+java -cp "lib/*" grammar.GrammarExtractor DataStructures.CollectionAttribute
+java -cp "lib/*" grammar.GrammarExtractor DataStructures.MapWrapper
+java -cp "lib/*" grammar.GrammarExtractor DataStructures.eiffel.Composite
