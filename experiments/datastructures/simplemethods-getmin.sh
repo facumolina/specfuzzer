f17f16e0367d3641907f1bb269f7bd91a86d5565
Facundo Molina <facu.molina7@gmail.com>
2021-06-18 19:38:31 -0300

    Adding useful scripts to run on simple methods

---
+++ experiments/datastructures/simplemethods-getmin.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# This script allows to generate invariants for the SimpleMethods.getMin case study by using a given technique: daikon, specfuzzer, gassert or evospex
+
+# Arguments
+technique=$1
+# Common 
+fqname='gassert.SimpleMethods'
+class='SimpleMethods'
+method='getMin'
+target_name='SimpleMethodsTesterDriver'
+# Daikon
+trace='daikon-outputs/'$target_name'.dtrace.gz'
+inv_file=$target_name'.inv.gz'
+# SpecFuzzer
+invs_to_fuzz=2000
+grammar='grammars/SimpleMethodsGrammar.json'
+
+# Verify that the required environment variables have been set
+[[ -z "$SPECFUZZER" ]] && { echo "> The environment variable SPECFUZZER is empty" ; exit 1; }
+
+# Run the technique
+echo '> Analyzing gassert.SimpleMethods.getMin with technique: '$technique
+
+# Daikon standalone
+if [ $technique == "daikon" ]
+then
+  echo '> Daikon'
+  ./experiments/datastructures/run-daikon.sh $trace $inv_file $class $method
+fi
+
+# SpecFuzzer
+if [ $technique = "specfuzzer" ]
+then
+  ./experiments/datastructures/run-specfuzzer.sh $target_name $grammar $invs_to_fuzz $class $method 10
+fi
+
+# GAssert
+if [ $technique == "gassert" ] 
+then
+  echo '> GAssert'
+  echo '> Go to $GASSERT/scripts and perform: '
+  echo '  ./run_gassert.sh GASSERT ?'
+fi
+
+# EvoSpex
+if [ $technique == "evospex" ]
+then
+  echo '> EvoSpex'
+  echo ' /run ?'
+fi
+
+
+echo ''
+echo '> Done!'
