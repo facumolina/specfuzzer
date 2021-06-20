f5a946c18829bb379d74050fb33f157f98744265
Facundo Molina <facu.molina7@gmail.com>
2021-06-20 13:23:50 -0300

    Remove unncessary vars from script

--- experiments/setup/basic-methods-setup.sh
+++ experiments/setup/basic-methods-setup.sh
@@ -5,12 +5,7 @@
 # Define vars
 fqname='gassert.BasicMethods'
 tester_base='BasicMethodsTester'
-omit_pattern=$tester_base'.*'
 driver_name='BasicMethodsTesterDriver'
-driver='testers.BasicMethodsTesterDriver'
-daikon_out=$SPECFUZZER'/daikon-outputs'
-cmp_file=$daikon_out'/BasicMethodsTesterDriver.decls-DynComp'
-objs_file='daikon-outputs/BasicMethodsTesterDriver-objects.xml'
 source_file='gassert/BasicMethods.java'
 
 ./experiments/setup/common.sh $fqname $tester_base $driver_name $source_file
