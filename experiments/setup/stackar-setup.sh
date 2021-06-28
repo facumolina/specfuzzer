d80cc8f3a15f5d109813047823b9da109854b439
Facundo Molina <facu.molina7@gmail.com>
2021-06-28 20:29:22 -0300

    Updating StackAr-push script

--- experiments/setup/stackar-setup.sh
+++ experiments/setup/stackar-setup.sh
@@ -5,12 +5,7 @@
 # Define vars
 fqname='DataStructures.daikon.StackAr'
 tester_base='StackArTester'
-omit_pattern=$tester_base'.*'
 driver_name='StackArTesterDriver'
-driver='testers.StackArTesterDriver'
-daikon_out=$SPECFUZZER'/daikon-outputs'
-cmp_file=$daikon_out'/StackArTesterDriver.decls-DynComp'
-objs_file='daikon-outputs/StackArTesterDriver-objects.xml'
 source_file='DataStructures/daikon/StackAr.java'
 
 ./experiments/setup/common.sh $fqname $tester_base $driver_name $source_file
