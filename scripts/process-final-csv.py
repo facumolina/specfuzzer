c723c7b7f08af53524bc0d69d49672b92ef21cb6
Facundo Molina <facu.molina7@gmail.com>
2021-06-29 15:02:56 -0300

    Computing average number of specs for SpecFuzzer

--- scripts/process-final-csv.py
+++ scripts/process-final-csv.py
@@ -2,44 +2,15 @@ import pandas as pd
 import os.path
 import sys
 
-# Load file and arguments
+# Load a csv file with the results of multiple executions.
 df = pd.read_csv(sys.argv[1])
 
-# Calculate a print some useful information
-print("")
-total_invs = len(df['invariant'].value_counts())
-unique_ppt = df.ppt.unique()
-unique_mut = df.mutant.unique()
-print("Number of invariants killing mutants: ", total_invs)
-print()
-print("Unique program points: "+str(len(unique_ppt)))
-print(list(unique_ppt))
-print()
-print("Unique mutants: "+str(len(unique_mut)))
-print()
+# Calculate and print some useful information
+average_inferred = df['inferred_nf'].mean()
+average_filtered_ma = df['filtered_ma'].mean()
+average_filtered_buckets = df['filtered_buckets'].mean()
 
-column_names = ["invariant", "fails", "fst_it", "total_its"]
-
-
-for ppt in unique_ppt:
-    ppt_df = pd.DataFrame(columns = column_names)
-    for index, row in df.iterrows():
-        if (row['ppt']==ppt and ppt_df[ppt_df['invariant']==row['invariant']].empty):
-            min_it = df[df['invariant']==row['invariant']]['iteration'].min()
-            total_it = len(df[df['invariant']==row['invariant']]['iteration'].unique())
-            fails = len(df[(df['invariant']==row['invariant']) & (df['iteration']==min_it)])
-            row_to_add = {
-            "invariant":[row['invariant']],
-            "fails":[fails],
-            "fst_it":[min_it],
-            "total_its":[total_it]
-            }
-            ppt_df = ppt_df.append(pd.DataFrame(row_to_add), ignore_index=True)
-
-    print("Program Point: "+ppt)
-    print("     Invariants sorted by mutation killing ability (times that failed in the program point)")
-    ka = ppt_df.sort_values(by=['fails'], ascending=False, ignore_index=True)
-    pd.set_option('max_colwidth', 1000)
-    pd.set_option("display.max_rows", None)
-    print(ka[['invariant','fails']])
-    print()
+print("Executions: %d" % len(df))
+print("Avg inferred: {:.2f}".format(average_inferred))
+print("Avg filtered ma: {:.2f}".format(average_filtered_ma))
+print("Avg filtered buckets: {:.2f}".format(average_filtered_buckets))
