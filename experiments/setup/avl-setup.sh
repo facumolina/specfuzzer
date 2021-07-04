e91e118407c47e6fad9778ec87f1bcd95948ab1d
Facundo Molina <facu.molina7@gmail.com>
2021-07-04 16:28:20 -0300

    AvlTreeList setup script cleaned

--- experiments/setup/avl-setup.sh
+++ experiments/setup/avl-setup.sh
@@ -5,12 +5,7 @@
 # Define vars
 fqname='DataStructures.AvlTreeList'
 tester_base='AvlTreeListTester'
-omit_pattern=$tester_base'.*'
 driver_name='AvlTreeListTesterDriver'
-driver='testers.AvlListTesterDriver'
-daikon_out=$SPECFUZZER'/daikon-outputs'
-cmp_file=$daikon_out'/AvlTreeListTesterDriver.decls-DynComp'
-objs_file='daikon-outputs/AvlTreeListTesterDriver-objects.xml'
 source_file='DataStructures/AvlTreeList.java'
 
 ./experiments/setup/common.sh $fqname $tester_base $driver_name $source_file
