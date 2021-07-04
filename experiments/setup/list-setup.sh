f2951cfaad349940be8d864d4031069ea4914def
Facundo Molina <facu.molina7@gmail.com>
2021-07-04 16:26:22 -0300

    List setup script cleaned

--- experiments/setup/list-setup.sh
+++ experiments/setup/list-setup.sh
@@ -5,12 +5,7 @@
 # Define vars
 fqname='DataStructures.List'
 tester_base='ListTester'
-omit_pattern=$tester_base'.*'
 driver_name='ListTesterDriver'
-driver='testers.ListTesterDriver'
-daikon_out=$SPECFUZZER'/daikon-outputs'
-cmp_file=$daikon_out'/ListTesterDriver.decls-DynComp'
-objs_file='daikon-outputs/ListTesterDriver-objects.xml'
 source_file='DataStructures/List.java'
 
 ./experiments/setup/common.sh $fqname $tester_base $driver_name $source_file
